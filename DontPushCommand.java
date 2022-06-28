892e21b8b041ef39d40b5ef46680ed489d46b4a5
KonitoT1 <63659415+KonitoT1@users.noreply.github.com>
2022-06-28 18:59:52 +0300

    Create DontPushCommand.java

---
+++ DontPushCommand.java
@@ -0,0 +1,24 @@
+package fmi;
+
+public class DontPushCommand implements Command{
+
+	private Trainee trainee; //reciever
+	
+	public DontPushCommand(Trainee trainee)
+	{	
+		this.trainee = trainee;
+	}
+	
+	public void execute() {
+		System.out.println("Dont push.");	
+		this.trainee.dontPush();
+		
+	}
+
+	@Override
+	public void undo() {
+		this.trainee.getUp();
+		
+	}
+
+}
